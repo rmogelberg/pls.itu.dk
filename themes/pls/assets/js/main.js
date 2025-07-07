89aa751a0aefc80ffa152d24ed47fb713c1b7961
Alessandro Bruni <brun@itu.dk>
2025-07-07 13:47:10 +0200

    css cards

--- themes/pls/assets/js/main.js
+++ themes/pls/assets/js/main.js
@@ -1,8 +1,10 @@
 window.addEventListener('load', () => {
     const hamburger = document.getElementById('hamburger');
+    const blur = document.getElementById('blur');
     const nav = document.getElementById('mainnav');
     hamburger.addEventListener('click', () => {
     nav.classList.toggle('open');
+    blur.classList.toggle('open');
     hamburger.classList.toggle('open');
     });
 });
